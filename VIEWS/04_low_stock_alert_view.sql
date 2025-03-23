0247316ecdb5c0c9a0f5a38b9d67e343d8406eb6
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:48:57 -0400

    Create 04_low_stock_alert_view.sql

---
+++ VIEWS/04_low_stock_alert_view.sql
@@ -0,0 +1,13 @@
+-- View 4: Low Stock Alert View
+CREATE OR REPLACE VIEW low_stock_alert_view AS
+SELECT 
+    v.VariationID,
+    p.ProductName,
+    c.ColorName,
+    s.SizeName,
+    v.StockQuantity
+FROM ProductVariations v
+JOIN Products p ON v.ProductID = p.ProductID
+JOIN Colors c ON v.ColorID = c.ColorID
+JOIN SizeOptions s ON v.SizeID = s.SizeID
+WHERE v.StockQuantity < 5;
