8bc727a4251ae972a42adb9a33a00feb1ddd3a91
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:54:42 -0400

    Create 14_brand_product_summary_view.sql

---
+++ VIEWS/14_brand_product_summary_view.sql
@@ -0,0 +1,8 @@
+-- View 14: Brand Product Summary View
+CREATE OR REPLACE VIEW brand_product_summary_view AS
+SELECT 
+    b.BrandName,
+    COUNT(p.ProductID) AS TotalProducts
+FROM Brands b
+JOIN Products p ON b.BrandID = p.BrandID
+GROUP BY b.BrandName;
