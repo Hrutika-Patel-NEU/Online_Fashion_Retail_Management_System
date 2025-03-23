763dfe6311105e55071e8f544f2eb74818a06e4d
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:55:51 -0400

    Create 16_admin_all_orders_view.sql

---
+++ VIEWS/16_admin_all_orders_view.sql
@@ -0,0 +1,12 @@
+-- View 16: Admin View - All Orders with Customer Info
+CREATE OR REPLACE VIEW admin_all_orders_view AS
+SELECT 
+    o.OrderID,
+    u.FirstName || ' ' || u.LastName AS CustomerName,
+    u.Email,
+    o.Status,
+    o.PaymentMethod,
+    o.PaymentStatus,
+    o.CreatedAt AS OrderDate
+FROM Orders o
+JOIN Users u ON o.UserID = u.UserID;
