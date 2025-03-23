58f69de63322b509fd693121acb6ef46722a9277
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:52:29 -0400

    Create 10_top_selling_products_view.sql

---
+++ VIEWS/10_top_selling_products_view.sql
@@ -0,0 +1,12 @@
+-- View 10: Top Selling Products View
+CREATE OR REPLACE VIEW top_selling_products_view AS
+SELECT 
+    p.ProductID,
+    p.ProductName,
+    SUM(oi.Quantity) AS TotalSold,
+    SUM(oi.SoldPrice * oi.Quantity) AS TotalRevenue
+FROM OrderLineItems oi
+JOIN ProductVariations v ON oi.VariationID = v.VariationID
+JOIN Products p ON v.ProductID = p.ProductID
+GROUP BY p.ProductID, p.ProductName
+ORDER BY TotalSold DESC;
