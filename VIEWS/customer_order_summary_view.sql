7bb686b040bd34d6a7e6eb1a7a3eae0a30d68ec9
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:45:14 -0400

    Create customer_order_summary_view.sql

---
+++ VIEWS/customer_order_summary_view.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW customer_order_summary_view AS
+SELECT 
+    u.UserID,
+    u.FirstName || ' ' || u.LastName AS CustomerName,
+    COUNT(o.OrderID) AS TotalOrders
+FROM Users u
+LEFT JOIN Orders o ON u.UserID = o.UserID
+GROUP BY u.UserID, u.FirstName, u.LastName;
