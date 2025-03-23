0bb83668f3fb96d3dbc70c7a5b2630e227156113
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:52:04 -0400

    Create 09_user_saved_addresses_view.sql

---
+++ VIEWS/09_user_saved_addresses_view.sql
@@ -0,0 +1,13 @@
+-- View 9: User Saved Addresses View
+CREATE OR REPLACE VIEW user_saved_addresses_view AS
+SELECT 
+    u.UserID,
+    u.FirstName || ' ' || u.LastName AS CustomerName,
+    a.AddressLine1,
+    a.AddressLine2,
+    a.City,
+    a.State,
+    a.Country,
+    a.ZipCode
+FROM Address a
+JOIN Users u ON a.UserID = u.UserID;
