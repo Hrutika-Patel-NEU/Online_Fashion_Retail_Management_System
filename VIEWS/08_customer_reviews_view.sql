7248f59eac9cc954b6f452b516003a1bb91bdff8
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-03-23 17:51:25 -0400

    Create 08_customer_reviews_view.sql

---
+++ VIEWS/08_customer_reviews_view.sql
@@ -0,0 +1,12 @@
+-- View 8: Customer Reviews View
+CREATE OR REPLACE VIEW customer_reviews_view AS
+SELECT 
+    r.ReviewID,
+    u.FirstName || ' ' || u.LastName AS CustomerName,
+    p.ProductName,
+    r.Rating,
+    r.ReviewComment,
+    r.CreatedAt
+FROM Reviews r
+JOIN Users u ON r.UserID = u.UserID
+JOIN Products p ON r.ProductID = p.ProductID;
