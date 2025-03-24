a511bf2042ab623d35de0e1ccab40f771d01c85d
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-03-24 18:53:03 -0400

    Product variations created.

---
+++ DML/DATA/12_ProductVariations_Data.sql
@@ -0,0 +1,200 @@
+-- Variations for ProductID 1 (Urban Graphic Tee)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (1, 1, 2, 1, 100);  -- S, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (2, 1, 3, 2, 80);   -- M, White
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (3, 1, 4, 3, 60);   -- L, Red
+
+-- Variations for ProductID 2 (Classic Polo)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (4, 2, 3, 4, 50);   -- M, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (5, 2, 4, 1, 40);   -- L, Black
+
+-- Variations for ProductID 3 (Sporty Mesh Tank)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (6, 3, 2, 5, 70);   -- S, Green
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (7, 3, 3, 6, 90);   -- M, Yellow
+
+-- Variations for ProductID 4 (Outdoor Adventure Shirt)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (8, 4, 3, 9, 45);   -- M, Grey
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (9, 4, 4, 13, 35);  -- L, Navy
+
+-- Variations for ProductID 5 (Evening Blouse)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (10, 5, 2, 7, 25);  -- S, Pink
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (11, 5, 3, 8, 20);  -- M, Purple
+
+-- Variations for ProductID 6 (Elegant Evening Gown)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (12, 6, 4, 3, 15);  -- L, Red
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (13, 6, 5, 4, 10);  -- XL, Blue
+
+-- Variations for ProductID 7 (Boho Summer Dress)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (14, 7, 2, 6, 20);  -- S, Yellow
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (15, 7, 3, 3, 25);  -- M, Red
+
+-- Variations for ProductID 8 (Vintage Velvet Dress)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (16, 8, 3, 14, 12); -- M, Maroon
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (17, 8, 4, 1, 10);  -- L, Black
+
+-- Variations for ProductID 9 (Street Chic Dress)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (18, 9, 2, 2, 30);  -- S, White
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (19, 9, 3, 4, 20);  -- M, Blue
+
+-- Variations for ProductID 10 (Business Casual Dress)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (20, 10, 3, 9, 18); -- M, Grey
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (21, 10, 4, 12, 15);-- L, Beige
+
+-- Variations for ProductID 11 (Urban Joggers)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (22, 11, 3, 1, 50);   -- M, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (23, 11, 4, 2, 40);   -- L, White
+
+-- Variations for ProductID 12 (Classic Chinos)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (24, 12, 3, 9, 60);   -- M, Grey
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (25, 12, 4, 10, 45);  -- L, Brown
+
+-- Variations for ProductID 13 (Vintage Denim Jeans)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (26, 13, 3, 4, 55);   -- M, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (27, 13, 4, 14, 35);  -- L, Maroon
+
+-- Variations for ProductID 14 (Glitzy Sequin Skirt)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (28, 14, 2, 3, 20);   -- S, Red
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (29, 14, 3, 7, 18);   -- M, Pink
+
+-- Variations for ProductID 15 (Sporty Track Pants)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (30, 15, 3, 5, 70);   -- M, Green
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (31, 15, 4, 6, 60);   -- L, Yellow
+
+-- Variations for ProductID 16 (All-Weather Explorer Jacket)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (32, 16, 4, 13, 22);  -- L, Navy
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (33, 16, 5, 9, 18);   -- XL, Grey
+
+-- Variations for ProductID 17 (Executive Wool Coat)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (34, 17, 4, 1, 12);   -- L, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (35, 17, 5, 2, 10);   -- XL, White
+
+-- Variations for ProductID 18 (Heated Tech Jacket)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (36, 18, 4, 15, 8);   -- L, Teal
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (37, 18, 5, 1, 6);    -- XL, Black
+
+-- Variations for ProductID 19 (Evening Shawl)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (38, 19, 8, 7, 25);   -- Free Size, Pink
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (39, 19, 8, 3, 20);   -- Free Size, Red
+
+-- Variations for ProductID 20 (Retro Denim Jacket)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (40, 20, 3, 4, 30);   -- M, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (41, 20, 4, 10, 25);  -- L, Brown
+
+-- Variations for ProductID 21 (Urban Sneakers)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (42, 21, 3, 1, 60);   -- M, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (43, 21, 4, 4, 50);   -- L, Blue
+
+-- Variations for ProductID 22 (Classic Leather Loafers)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (44, 22, 3, 9, 40);   -- M, Grey
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (45, 22, 4, 10, 30);  -- L, Brown
+
+-- Variations for ProductID 23 (Retro High-Tops)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (46, 23, 3, 3, 35);   -- M, Red
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (47, 23, 4, 2, 28);   -- L, White
+
+-- Variations for ProductID 24 (Glitter Pumps)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (48, 24, 2, 7, 25);   -- S, Pink
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (49, 24, 3, 8, 20);   -- M, Purple
+
+-- Variations for ProductID 25 (Trail Running Shoes)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (50, 25, 3, 5, 40);   -- M, Green
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (51, 25, 4, 6, 35);   -- L, Yellow
+
+-- Variations for ProductID 26 (Retro Round Sunglasses)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (52, 26, 8, 1, 100);  -- Free Size, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (53, 26, 8, 2, 90);   -- Free Size, White
+
+-- Variations for ProductID 27 (Crystal Clutch Bag)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (54, 27, 8, 3, 40);   -- Free Size, Red
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (55, 27, 8, 12, 35);  -- Free Size, Beige
+
+-- Variations for ProductID 28 (Premium Leather Wallet)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (56, 28, 8, 10, 45);  -- Free Size, Brown
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (57, 28, 8, 9, 50);   -- Free Size, Grey
+
+-- Variations for ProductID 29 (Sports Wristbands Set)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (58, 29, 8, 4, 80);   -- Free Size, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (59, 29, 8, 6, 70);   -- Free Size, Yellow
+
+-- Variations for ProductID 30 (Bluetooth Beanie Hat)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (60, 30, 8, 1, 40);   -- Free Size, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (61, 30, 8, 15, 30);  -- Free Size, Teal
+
+-- Variations for ProductID 31 (Performance Running Tee)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (62, 31, 3, 4, 60);   -- M, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (63, 31, 4, 5, 55);   -- L, Green
+
+-- Variations for ProductID 32 (Seamless Yoga Leggings)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (64, 32, 3, 1, 50);   -- M, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (65, 32, 4, 15, 45);  -- L, Teal
+
+-- Variations for ProductID 33 (Smart Fitness Shirt)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (66, 33, 3, 2, 35);   -- M, White
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (67, 33, 4, 4, 30);   -- L, Blue
+
+-- Variations for ProductID 34 (Quick-Dry Running Jacket)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (68, 34, 4, 13, 28);  -- L, Navy
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (69, 34, 5, 9, 25);   -- XL, Grey
+
+-- Variations for ProductID 35 (Urban Active Shorts)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (70, 35, 2, 3, 40);   -- S, Red
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (71, 35, 3, 6, 35);   -- M, Yellow
+
+-- Variations for ProductID 36 (Classic Two-Piece Bikini)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (72, 36, 2, 7, 25);   -- S, Pink
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (73, 36, 3, 8, 20);   -- M, Purple
+
+-- Variations for ProductID 37 (Luxury One-Piece Swimsuit)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (74, 37, 3, 2, 18);   -- M, White
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (75, 37, 4, 12, 15);  -- L, Beige
+
+-- Variations for ProductID 38 (Urban Swim Trunks)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (76, 38, 3, 4, 30);   -- M, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (77, 38, 4, 1, 28);   -- L, Black
+
+-- Variations for ProductID 39 (Smart UV-Alert Swimwear)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (78, 39, 3, 6, 22);   -- M, Yellow
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (79, 39, 4, 4, 18);   -- L, Blue
+
+-- Variations for ProductID 40 (Athletic Racerback Swimsuit)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (80, 40, 3, 13, 20);  -- M, Navy
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (81, 40, 4, 1, 18);   -- L, Black
+
+-- Variations for ProductID 41 (Cotton Kurta Set)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (82, 41, 3, 4, 40);   -- M, Blue
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (83, 41, 4, 1, 35);   -- L, Black
+
+-- Variations for ProductID 42 (Embroidered Saree)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (84, 42, 8, 3, 20);   -- Free Size, Red
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (85, 42, 8, 12, 18);  -- Free Size, Beige
+
+-- Variations for ProductID 43 (Formal Nehru Jacket)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (86, 43, 3, 9, 30);   -- M, Grey
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (87, 43, 4, 10, 25);  -- L, Brown
+
+-- Variations for ProductID 44 (Vintage Banarasi Dupatta)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (88, 44, 8, 7, 22);   -- Free Size, Pink
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (89, 44, 8, 14, 20);  -- Free Size, Maroon
+
+-- Variations for ProductID 45 (Kids Ethnic Kurti)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (90, 45, 9, 6, 30);   -- Kids-S, Yellow
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (91, 45, 10, 3, 28);  -- Kids-M, Red
+
+-- Variations for ProductID 46 (Slim Fit Formal Suit)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (92, 46, 4, 1, 18);   -- L, Black
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (93, 46, 5, 9, 15);   -- XL, Grey
+
+-- Variations for ProductID 47 (Formal Pencil Skirt)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (94, 47, 2, 2, 25);   -- S, White
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (95, 47, 3, 1, 20);   -- M, Black
+
+-- Variations for ProductID 48 (Embellished Formal Gown)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (96, 48, 4, 7, 15);   -- L, Pink
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (97, 48, 5, 3, 10);   -- XL, Red
+
+-- Variations for ProductID 49 (Smart Blazer with NFC)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (98, 49, 4, 10, 12);  -- L, Brown
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (99, 49, 5, 2, 10);   -- XL, White
+
+-- Variations for ProductID 50 (Urban Formal Trousers)
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (100, 50, 3, 9, 30);  -- M, Grey
+INSERT INTO ProductVariations (VariationID, ProductID, SizeID, ColorID, StockQuantity) VALUES (101, 50, 4, 13, 25); -- L, Navy
