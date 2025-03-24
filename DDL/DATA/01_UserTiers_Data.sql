04aa1115dd55e21b64c1e19eb1c21ef6b91ffa95
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-03-24 16:46:58 -0400

    Data directory created. Added data to User Tiers.

---
+++ DDL/DATA/01_UserTiers_Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (1, 'Standard', 0);
+INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (2, 'Bronze', 5);
+INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (3, 'Silver', 10);
+INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (4, 'Gold', 15);
+INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (5, 'Platinum', 20);
