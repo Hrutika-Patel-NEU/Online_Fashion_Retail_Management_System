80a7c8597c772eb9a73a14f90c539eb9bcf07ed0
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-03-24 18:46:35 -0400

    Color data added to the table.

---
+++ DML/DATA/11_Colors_Data.sql
@@ -0,0 +1,15 @@
+INSERT INTO Colors (ColorID, ColorName) VALUES (1, 'Black');
+INSERT INTO Colors (ColorID, ColorName) VALUES (2, 'White');
+INSERT INTO Colors (ColorID, ColorName) VALUES (3, 'Red');
+INSERT INTO Colors (ColorID, ColorName) VALUES (4, 'Blue');
+INSERT INTO Colors (ColorID, ColorName) VALUES (5, 'Green');
+INSERT INTO Colors (ColorID, ColorName) VALUES (6, 'Yellow');
+INSERT INTO Colors (ColorID, ColorName) VALUES (7, 'Pink');
+INSERT INTO Colors (ColorID, ColorName) VALUES (8, 'Purple');
+INSERT INTO Colors (ColorID, ColorName) VALUES (9, 'Grey');
+INSERT INTO Colors (ColorID, ColorName) VALUES (10, 'Brown');
+INSERT INTO Colors (ColorID, ColorName) VALUES (11, 'Orange');
+INSERT INTO Colors (ColorID, ColorName) VALUES (12, 'Beige');
+INSERT INTO Colors (ColorID, ColorName) VALUES (13, 'Navy');
+INSERT INTO Colors (ColorID, ColorName) VALUES (14, 'Maroon');
+INSERT INTO Colors (ColorID, ColorName) VALUES (15, 'Teal');
