09062158df9d7f13d3b1b84a9a2a68af2bda5eea
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-03-24 19:52:57 -0400

    User tiers auto cleanup for data insertion added.

--- DML/01_UserTiers_Data.sql
+++ DML/01_UserTiers_Data.sql
@@ -1,3 +1,15 @@
+BEGIN
+   -- Cleanup existing data
+   DELETE FROM UserTiers;
+   COMMIT;
+   DBMS_OUTPUT.PUT_LINE('UserTiers table cleaned successfully.');
+EXCEPTION
+   WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('Error during cleanup: ' || SQLERRM);
+END;
+/
+
+
 INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (1, 'Standard', 0);
 INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (2, 'Bronze', 5);
 INSERT INTO UserTiers (TierID, TierName, DiscountRate) VALUES (3, 'Silver', 10);
