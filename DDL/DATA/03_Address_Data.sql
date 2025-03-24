78cdc77d083ed8dc99bd94d431a9e5288aea5450
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-03-24 17:13:10 -0400

    User addresses data created.

---
+++ DDL/DATA/03_Address_Data.sql
@@ -0,0 +1,67 @@
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(1, 1, '101 Red Lane', 'Apt 5', '560100', 'Bangalore', 'Karnataka', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(2, 2, '202 Blue Drive', 'Floor 6', '110034', 'New Delhi', 'Delhi', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(3, 3, '303 Green Street', NULL, '500081', 'Hyderabad', 'Telangana', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(4, 4, '404 Yellow Boulevard', 'Suite 12', '700091', 'Kolkata', 'West Bengal', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(5, 5, '505 Pink Avenue', 'Building 9', '400076', 'Mumbai', 'Maharashtra', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(6, 6, '606 Silver Road', NULL, '380015', 'Ahmedabad', 'Gujarat', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(7, 7, '707 Gold Court', 'Apt 22', '600040', 'Chennai', 'Tamil Nadu', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(8, 8, '808 Bronze Path', 'Block 3', '122018', 'Gurgaon', 'Haryana', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(9, 9, '909 Copper Line', 'Villa 7', '411057', 'Pune', 'Maharashtra', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(10, 10, '1010 Platinum Way', 'Penthouse', '560103', 'Bangalore', 'Karnataka', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(11, 11, '1111 Quartz Road', 'Unit 31', '201301', 'Noida', 'Uttar Pradesh', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(12, 12, '1212 Opal Court', 'Level 2', '208022', 'Kanpur', 'Uttar Pradesh', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(13, 13, '1313 Amber Street', NULL, '302006', 'Jaipur', 'Rajasthan', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(14, 14, '1414 Sapphire Ave', 'Top Floor', '492001', 'Raipur', 'Chhattisgarh', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(15, 15, '1515 Jade Lane', 'Office 404', '834002', 'Ranchi', 'Jharkhand', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(16, 16, '1616 Diamond Drive', NULL, '781005', 'Guwahati', 'Assam', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(17, 17, '1717 Ruby Road', 'Flat 18', '700029', 'Kolkata', 'West Bengal', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(18, 18, '1818 Emerald Blvd', 'Suite 88', '110025', 'New Delhi', 'Delhi', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(19, 19, '1919 Topaz Trail', 'Unit 55', '682021', 'Cochin', 'Kerala', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(20, 20, '2020 Pearl Pathway', 'Villa 19', '605001', 'Pondicherry', 'Puducherry', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(21, 21, '2121 Granite Glade', NULL, '403001', 'Panaji', 'Goa', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(22, 22, '2222 Limestone Lane', 'Bungalow 6', '500049', 'Hyderabad', 'Telangana', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(23, 23, '2323 Marble March', 'Apt 101', '411045', 'Pune', 'Maharashtra', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(24, 24, '2424 Basalt Bank', 'Loft 302', '110048', 'New Delhi', 'Delhi', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(25, 25, '2525 Flint Field', NULL, '380009', 'Ahmedabad', 'Gujarat', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(26, 26, '2626 Chalk Chateau', 'Penthouse', '751015', 'Bhubaneswar', 'Odisha', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(27, 27, '2727 Slate Street', 'Floor 9', '560037', 'Bangalore', 'Karnataka', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(28, 28, '2828 Coal Crescent', 'Building 2', '400093', 'Mumbai', 'Maharashtra', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(29, 29, '2929 Quartzite Quay', 'Suite 150', '110011', 'New Delhi', 'Delhi', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(30, 30, '3030 Peat Path', 'Unit 202', '682016', 'Cochin', 'Kerala', 'India');
+-- Multiple addresses for some users
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(31, 1, '3101 Red Lane', 'Apt 15', '560100', 'Bangalore', 'Karnataka', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(32, 2, '3202 Blue Drive', 'Floor 16', '110034', 'New Delhi', 'Delhi', 'India');
+INSERT INTO Address (AddressID, UserID, AddressLine1, AddressLine2, ZipCode, City, State, Country) VALUES 
+(33, 3, '3303 Green Street', 'Block 5', '500081', 'Hyderabad', 'Telangana', 'India');
