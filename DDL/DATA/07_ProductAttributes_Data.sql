1f59d97a9616bc227ac479c4082dd9f3d5241703
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-03-24 18:16:17 -0400

    Data to the product attributes table has been added

---
+++ DDL/DATA/07_ProductAttributes_Data.sql
@@ -0,0 +1,20 @@
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(1, 'Material');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(2, 'Fit');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(3, 'Sleeve Length');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(4, 'Neck Type');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(5, 'Pattern');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(6, 'Occasion');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(7, 'Length');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(8, 'Waist Rise');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(9, 'Closure Type');
+INSERT INTO ProductAttributes (AttributeID, AttributeName) VALUES 
+(10, 'Technology');
