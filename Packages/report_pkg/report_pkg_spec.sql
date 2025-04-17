51ed73ba96462ee57f91202fdec7c85ae6370831
Your Name <akhouri.a@northeastern.edu>
2025-04-16 22:09:05 -0400

    added report management package specs

---
+++ Packages/report_pkg/report_pkg_spec.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE PACKAGE report_pkg IS
+  PROCEDURE sp_customer_order_summary(p_userid IN USERS.USERID%TYPE);
+  PROCEDURE sp_product_inventory_report;
+  PROCEDURE sp_top_selling_products;
+  PROCEDURE sp_low_stock_alerts;
+  PROCEDURE sp_admin_all_orders;
+END report_pkg;
+/
