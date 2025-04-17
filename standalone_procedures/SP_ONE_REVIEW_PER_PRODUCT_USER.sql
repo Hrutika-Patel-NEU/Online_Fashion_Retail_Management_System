19f7621402dea0fa9d451e3e7c363adad9e5b024
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-04-16 20:58:44 -0400

    Updated SP for one review per customer per product.

--- standalone_procedures/SP_ONE_REVIEW_PER_PRODUCT_USER.sql
+++ standalone_procedures/SP_ONE_REVIEW_PER_PRODUCT_USER.sql
@@ -1,16 +1,20 @@
-create or replace PROCEDURE sp_one_review_per_product_user (
-  p_userid    IN USERS.USERID%TYPE,
-  p_productid IN PRODUCTS.PRODUCTID%TYPE
+CREATE OR REPLACE PROCEDURE sp_one_review_per_product_user (
+  p_userid    IN USERS.USERID%TYPE,       --  ID of the user
+  p_productid IN PRODUCTS.PRODUCTID%TYPE  --  ID of the product
 ) IS
-  v_count NUMBER;
+  v_count NUMBER;                         -- Variable for the count of existing reviews
 BEGIN
+  -- Checking how many reviews exist by the user for the given product
   SELECT COUNT(*) INTO v_count
   FROM REVIEWS
-  WHERE USERID = p_userid AND PRODUCTID = p_productid;
+  WHERE USERID = p_userid 
+    AND PRODUCTID = p_productid;
 
+  -- If at least one review exists, raise an error
   IF v_count > 0 THEN
     RAISE_APPLICATION_ERROR(-28007, 'User has already submitted a review for this product.');
   ELSE
+    -- If no review exists, allow user to proceed 
     DBMS_OUTPUT.PUT_LINE('No existing review. Allowed to review.');
   END IF;
 END;
