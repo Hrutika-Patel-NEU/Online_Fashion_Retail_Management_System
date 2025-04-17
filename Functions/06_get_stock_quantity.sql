74eaef8e0ad507d5aa5102a0fae6ffd8f0de12f8
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-04-16 20:23:22 -0400

    Added EXECUTE grants for get_stock_quantity to admin, user, and report roles

--- Functions/06_get_stock_quantity.sql
+++ Functions/06_get_stock_quantity.sql
@@ -33,3 +33,8 @@ EXCEPTION
     WHEN TOO_MANY_ROWS THEN
         RAISE_APPLICATION_ERROR(-20005, 'Duplicate product variation found.');
 END;
+
+--Added EXECUTE grants for get_stock_quantity to admin, user, and report roles
+GRANT EXECUTE ON get_stock_quantity TO ecom_app_admin;
+GRANT EXECUTE ON get_stock_quantity TO ecom_app_user;
+GRANT EXECUTE ON get_stock_quantity TO ecom_app_report_user;
