21776d6ed2af928f3fdaff90d7c7c42dc8139a85
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-04-16 20:21:04 -0400

    Added EXECUTE grants for calculate_discounted_price to admin and user roles only

--- Functions/04_calculate_discounted_price.sql
+++ Functions/04_calculate_discounted_price.sql
@@ -16,3 +16,7 @@ EXCEPTION
     WHEN OTHERS THEN
         RAISE_APPLICATION_ERROR(-20004, 'Error calculating discounted price');
 END;
+
+--Added EXECUTE grants for calculate_discounted_price to admin and user roles only
+GRANT EXECUTE ON calculate_discounted_price TO ecom_app_admin;
+GRANT EXECUTE ON calculate_discounted_price TO ecom_app_user;
