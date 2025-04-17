dc1834e6a65c6c09eacdf8053531cb1cdeb45265
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-04-16 21:05:45 -0400

    Updated SP on updating the stock of a product on order.

--- standalone_procedures/SP_UPDATE_STOCK_ON_ORDER.sql
+++ standalone_procedures/SP_UPDATE_STOCK_ON_ORDER.sql
@@ -1,12 +1,15 @@
-create or replace PROCEDURE sp_update_stock_on_order (
-  p_variationid IN PRODUCTVARIATIONS.VARIATIONID%TYPE,
-  p_quantity    IN NUMBER
+CREATE OR REPLACE PROCEDURE sp_update_stock_on_order (
+  p_variationid IN PRODUCTVARIATIONS.VARIATIONID%TYPE,  -- ID of the product variation
+  p_quantity    IN NUMBER                                -- Quantity to deduct from stock
 ) IS
 BEGIN
+  -- Updating the stock quantity by subtracting the ordered quantity
+  -- Updating the timestamp to reflect the change
   UPDATE PRODUCTVARIATIONS
-  SET STOCKQUANTITY = STOCKQUANTITY - p_quantity,
-      UPDATEDAT = SYSTIMESTAMP
-  WHERE VARIATIONID = p_variationid;
+  SET STOCKQUANTITY = STOCKQUANTITY - p_quantity,        -- Deduct quantity from stock
+      UPDATEDAT = SYSTIMESTAMP                           -- Record the update time
+  WHERE VARIATIONID = p_variationid;                     -- Apply to the correct variation
 
+  -- Success message
   DBMS_OUTPUT.PUT_LINE('Stock updated successfully.');
 END;
