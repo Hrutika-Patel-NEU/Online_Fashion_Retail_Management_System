4629000c4294c26ad15ecf2d787eef43a0a0d939
vishweshgk <gopikrishnan.v@northeastern.edu>
2025-04-16 20:50:02 -0400

    Updated Function for User tier based discounts.

--- standalone_procedures/SP_APPLY_USER_TIER_DISCOUNT.sql
+++ standalone_procedures/SP_APPLY_USER_TIER_DISCOUNT.sql
@@ -1,12 +1,15 @@
-create or replace FUNCTION sp_apply_user_tier_discount (
-  p_userid IN USERS.USERID%TYPE,
-  p_price  IN PRODUCTS.PRICE%TYPE
+CREATE OR REPLACE FUNCTION sp_apply_user_tier_discount (
+  p_userid IN USERS.USERID%TYPE,        -- User ID input parameter
+  p_price  IN PRODUCTS.PRICE%TYPE       -- Product price input parameter
 ) RETURN NUMBER IS
-  v_discount USERTIERS.DISCOUNTRATE%TYPE := 0;
+  v_discount USERTIERS.DISCOUNTRATE%TYPE := 0;  -- Variable to hold the user's discount rate
 BEGIN
+  -- Get the discount rate for the user based on their user tier
   SELECT UT.DISCOUNTRATE INTO v_discount
-  FROM USERS U JOIN USERTIERS UT ON U.USERTIERID = UT.TIERID
-  WHERE U.USERID = p_userid;
+  FROM USERS U 
+  JOIN USERTIERS UT ON U.USERTIERID = UT.TIERID  -- Join to get the tier's discount rate
+  WHERE U.USERID = p_userid;                     -- Match the user ID
 
+  -- Return the price after applying the discount
   RETURN p_price - (p_price * v_discount / 100);
 END;
